bed71bc86bb02758fd34d1611c3aebb5ef8e2493
Md. Samiul Islam Sakib <68787875+SakibSamiul@users.noreply.github.com>
2024-06-05 12:24:40 +0600

    Add files via upload

---
+++ Variable_dataTypes.py
@@ -0,0 +1,32 @@
+a = "Samiul Sakib"
+b = 24
+c = complex(7, 3)
+d = True
+print(a)
+print(b)
+
+a1 = 90
+print(c + a1)
+
+print("The type of a",type(a))
+print("The type of b",type(b))
+print("The type of c",type(c))
+print("The type of d",type(d))
+
+l = [1, 2, 3, 4, 5, [7, 8, 9], ["Samiul","Sakib"], ("Orange","Banana", "Mangoe")]
+print(l)
+
+t = (("Orange","Banana","Mangoe"))
+print(t)
+
+di = {"Name":"Samiul Sakib","Age":24}
+print(di)
+
+s = {1, 2, 3, 4, 5}
+print(s)
+
+
+print("The type of l",type(l))
+print("The type of t",type(t))
+print("The type of di",type(di))
+print("The type of s",type(s))
